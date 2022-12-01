c45e507eef1e595ebcda125e743d294b1306a125
Andrew Javidfar <44504197+javida1492@users.noreply.github.com>
2022-12-01 01:41:55 -0500

    Day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,28 @@
 with open("input.txt") as f:
     lines = f.readlines()
 
+    top_three_sums = []
     cur_sum = 0
-    max_sum = 0
 
     for line in lines:
         if line.strip():
             cur_sum += int(line)
         else:
-            if cur_sum > max_sum:
-                max_sum = cur_sum
+            if(len(top_three_sums) < 3):
+                top_three_sums.append(cur_sum)
+                top_three_sums.sort()
+            else :
+                top_three_sums.append(cur_sum)
+                top_three_sums.sort()
+                top_three_sums.pop(0)
+                # if cur_sum > top_three_sums[2]:
+                #     top_three_sums.pop(0)
+                #     top_three_sums.append(cur_sum)
+
             cur_sum = 0
         
+    max_sum = 0
+    for num in top_three_sums:
+        max_sum += num
+
     print(max_sum)
