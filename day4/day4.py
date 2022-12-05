2b6eb9e009a4a0621a87ddaf3bc822e42a7bd818
Andrew <javida1492@gmail.com>
2022-12-04 21:29:15 -0500

    Day 4 part 4

--- day4/day4.py
+++ day4/day4.py
@@ -7,7 +7,11 @@ with open("input.txt") as f:
     for line in lines:
         line = [int(x) for x in re.split(r'[,-]', line.strip('\n'))]
 
-        if line[0] <= line[2] and line[1] >= line[3]:
+        if line[0] <= line[2] and line[1] >= line[2]:
+            overlapped_pairs += 1
+        elif line[2] <= line[0] and line[3] >= line[0]:
+            overlapped_pairs += 1
+        elif line[0] <= line[2] and line[1] <= line[3] and line[1] >= line[2]:
             overlapped_pairs += 1
         elif line[2] <= line[0] and line[3] >= line[1]:
             overlapped_pairs += 1
