d8ce458939ebaba3cb1a42e7d8543a2bb2b4dd54
Andrew Javidfar <44504197+javida1492@users.noreply.github.com>
2022-12-12 23:21:33 -0500

    Day 7 part 1 & 2

--- day7/day7.py
+++ day7/day7.py
@@ -16,7 +16,12 @@ def process_cd(cd, root, current_node):
         else:
             return current_node
     else:
+        if not search.find(current_node, lambda node: node.name==cd, maxlevel=max_level):
+            #Create the folder
+            Node(cd, size=0, parent=current_node, type="folder")
+        # CD into the folder
         return search.find(current_node, lambda node: node.name==cd, maxlevel=max_level)
+
 
 def process_ls(line, current_node):
     if(line[0] == "dir"):
@@ -25,17 +30,36 @@ def process_ls(line, current_node):
     else:
         if not search.find(current_node, lambda node: node.name == line[1], maxlevel=max_level):
             Node(line[1], size=int(line[0]), parent=current_node, type="file")
-            current_node.size += int(line[0])
+            # current_node.size += int(line[0])
 
 def calculate_sizes(root):
     sizes = []
     for node in PostOrderIter(root):
-        if not node.size > 100000:
-            sizes.append(node.size)
-        if not node.name == "/":
+        if node.type == "file":
             node.parent.size += node.size
+        else:
+            # if node.size <= 100000:
+            sizes.append(node.size)
+            if not node.name == "/":
+                node.parent.size += node.size
 
     return sizes
+
+def binary_search(sizes, diff):
+    left= int(0)
+    right=int(len(sizes))
+
+    while left <= right:
+        middle = int(left + (right - left)/2)
+
+        if sizes[middle] > diff and sizes[middle-1] > diff:
+            right = middle - 1
+        elif sizes[middle] < diff and sizes[middle+1] < diff:
+            left = middle + 1
+        elif sizes[middle] > diff and sizes[middle-1] < diff:
+            return int(middle)
+        elif sizes[middle] < diff and sizes[middle+1] > diff:
+            return int(middle+1)
 
 with open("input.txt") as f:
     lines = f.readlines()
@@ -60,4 +84,18 @@ with open("input.txt") as f:
     sizes = sorted(calculate_sizes(root))
 
     # print(RenderTree(root))
-    print(RenderTree(root, style=AsciiStyle()).by_attr())
+    # print(RenderTree(root, style=AsciiStyle()).by_attr())
+    print(sizes)
+    total_size = 0
+    total_disk_space = 70000000
+
+    for size in sizes:
+        total_size += size
+
+    diff = total_disk_space - root.size
+    unused = 30000000
+    print(unused-diff)
+    idx = int(binary_search(sizes, unused-diff))
+    print("Total size: " + str(total_size))
+    print("Diff: " + str(total_disk_space - root.size))
+    print("Folder size to delete: " + str(sizes[idx]))
