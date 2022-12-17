a3a82732a65151ce930ad71714e17574f30f3b8d
Andrew Javidfar <44504197+javida1492@users.noreply.github.com>
2022-12-17 01:45:49 -0500

    Day 8 part 1

--- day8/day8.py
+++ day8/day8.py
@@ -1,13 +1,33 @@
-with open("day8/input.txt") as f:
+with open("input.txt") as f:
     lines = f.readlines()
 
+    col_size = len(lines[0].strip("\n"))
+    row_size = len(lines)
+
     grid = []
+    # Minimum visible cells will be the edges
+    visible = (row_size*2) + (col_size*2) - 4
 
     for line in lines:
         line = line.strip("\n")
         line = list(line)
         converted = [int(num) for num in line]
         grid.append(converted)
-        # print(converted)
 
-    print(grid)
+    # Iterate through inner cells
+    for row in range (1, row_size-1):
+        for col in range(1, col_size-1):
+            cell = grid[row][col]
+
+            # Get all horizontal and verticle trees
+            left = [grid[row][col-i] for i in range(1, col+1)]
+            right = [grid[row][col+i] for i in range(1, col_size-col)]
+            up = [grid[row-i][col] for i in range(1, row+1)]
+            down = [grid[row+i][col] for i in range(1, row_size-row)]
+
+            # Part 1
+            # Check if tallest tree on all sides is greater than current tree
+            if max(left) < cell or max(right) < cell or max(up) < cell or max(down) < cell:
+                visible += 1
+
+    print(visible)
